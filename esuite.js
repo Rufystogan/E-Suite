acb6967fcb85da47eb3a9eb284ccd1fcd3607e16
Rufystogan <81664107+Rufystogan@users.noreply.github.com>
2021-08-26 17:43:49 +0800

    Update esuite.js

--- esuite.js
+++ esuite.js
@@ -13,4 +13,68 @@
         let today = new Date().toLocaleDateString();
 
 //Display current date
-        currentDate.innerHTML = (today); 
+        currentDate.innerHTML = (today);
+
+//notification
+var submitClick = function(){
+  $.notify({
+        // options
+        title: '<strong>Your booking request has been sent</strong>',
+        message: "<br>Please wait for confirmation email from LiveWIRE Admin<br>You will be redirected to homepage in 3 seconds",
+        icon: 'glyphicon glyphicon-ok',
+        target: '_blank'
+        },{
+        // settings
+        element: 'body',
+        //position: null,
+        type: "success",
+        //allow_dismiss: true,
+        //newest_on_top: false,
+        showProgressbar: false,
+        placement: {
+                from: "top",
+                align: "right"
+        },
+        offset: 20,
+        spacing: 10,
+        z_index: 1031,
+        delay: 3300,
+        timer: 1000,
+        url_target: '_blank',
+        mouse_over: null,
+        animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutRight'
+        },
+        onShow: null,
+        onShown: null,
+        onClose: null,
+        onClosed: null,
+        icon_type: 'class',
+});
+  setTimeout(function () {
+    window.location=("index.html")
+  }, 3000);
+}
+
+//condition enabling Submit Button
+var inputs = $("form input, form select");
+
+var validateInputs = function validateInputs(inputs) {
+  var validForm = true;
+  inputs.each(function(index) {
+    var input = $(this);
+    if (!input.val()) {
+      $("#bookSubmit").attr("disabled", "disabled");
+      validForm = false;
+    }
+  });
+  return validForm;
+}
+
+//disable submit button
+inputs.change(function() {
+  if (validateInputs(inputs)) {
+    $("#bookSubmit").removeAttr("disabled");
+  }
+});
