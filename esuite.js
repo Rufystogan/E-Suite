143b0867a894538e8db9aa169a42799f5a627562
Rufystogan <81664107+Rufystogan@users.noreply.github.com>
2021-09-22 19:17:55 +0800

    Update esuite.js

--- esuite.js
+++ esuite.js
@@ -78,3 +78,20 @@ inputs.change(function() {
     $("#bookSubmit").removeAttr("disabled");
   }
 });
+
+ //disable previous datepick
+ $(function(){
+    var dtToday = new Date();
+    
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    
+    var minDate= year + '-' + month + '-' + day;
+    
+    $('#date').attr('min', minDate);
+});
